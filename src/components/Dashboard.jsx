b4b3447581a09a79e1af078a6cb5134f0fdda80d
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-11 17:03:05 +0200

    Finished the transaction chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import DashboardStatsGrid from "./DashboardStatsGrid";
+import TransactionChart from "./TransactionChart";
 
 const Dashboard = () => {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-col gap-4 m-4">
       <DashboardStatsGrid />
+      <TransactionChart />
     </div>
   );
 };
